261aeeda5376e7cb5de4a9884c3b07e192dfd0f7
Lexi Wei <49824793+lexinwei@users.noreply.github.com>
2023-08-24 10:02:07 +0800

    Delete adonis.R

--- 01.species_level/adonis.R
+++
@@ -1,6 +0,0 @@
-ado1 = adonis2(abuMat_T~Age + BMI + Term + Ethnicity + CST, data = bray_curtis_pcoa_df, method="bray", by = 'margin')
-
-anosim_result<-anosim(abuMat_T,bray_curtis_pcoa_df$Term,permutations = 999)
-plot(anosim_result, col = c('gray', colorTerm))
-mod <- betadisper(bray_curtis_dist, bray_curtis_pcoa_df$Term)
-permutest(mod)
